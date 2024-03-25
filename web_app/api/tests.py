cbbe21dffdd72cc69054b96c605556cefcdee299
Seok Yim(Noah) <seokyim8@gmail.com>
2024-03-24 20:53:31 -0400

    Added unit testing within tests.py for api.

--- web_app/api/tests.py
+++ web_app/api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class general_test(TestCase):
+    def setUp(self):
+        '''Setting up process before unit testing.'''
+        pass
+
+    def test_get_home(self):
+        '''Checking html element.'''
+        response = self.client.get("/home")
+        self.assertEqual(response.status_code, 200)
+
+    def tearDown(self) -> None:
+        '''Cleanup after unit testing.'''
+        return super().tearDown()
