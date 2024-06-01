fec1909c91f501701c36bc70ce7289fa4fb84b22
Seok Yim <seokyim8@gmail.com>
2024-06-01 19:23:14 -0400

    fix issues with react omponents not rendering properly

--- web_app/frontend/src/components/App.js
+++ web_app/frontend/src/components/App.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        return <h1>Testing React Code</h1>;
-    }
+  render() {
+    return (
+      <div>
+        <h1>This is the App.js KKKKKK</h1>
+      </div>
+    );
+  }
 }
 
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 render(<App />, appDiv);
