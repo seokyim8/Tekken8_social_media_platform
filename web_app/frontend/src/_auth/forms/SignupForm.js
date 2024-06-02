992ab4aac865e0657e583995a52c2ca4ad41c52b
Seok Yim <seokyim8@gmail.com>
2024-06-02 19:15:10 -0400

    Fix rendering issues with tailwind in react + webpack

--- web_app/frontend/src/_auth/forms/SignupForm.js
+++ web_app/frontend/src/_auth/forms/SignupForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SignupForm = () => {
     return (
         <div>
-            SignupForm
+            <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Dark</button>
         </div>
     );
 }
--- web_app/frontend/webpack.config.js
+++ web_app/frontend/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
