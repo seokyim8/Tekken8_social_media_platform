8d5abb02ad51f143b27ae5141df93a7e50dbc34d
Seok Yim <seokyim8@gmail.com>
2024-06-03 22:04:03 -0400

    Adjusted tests

--- web_app/api/tests.py
+++ web_app/api/tests.py
@@ -7,10 +7,12 @@ class general_test(TestCase):
         '''Setting up process before unit testing.'''
         pass
 
-    def test_get_home(self):
+    def test_login_page(self):
         '''Checking html element.'''
-        response = self.client.get("/temp/home")
+        response = self.client.get("/sign-in")
         self.assertEqual(response.status_code, 200)
+    
+    # TODO: Include testing for signing in and out (entering home page, etc.)
 
     def tearDown(self) -> None:
         '''Cleanup after unit testing.'''
