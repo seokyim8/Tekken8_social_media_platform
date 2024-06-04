4a106a19f7aa590082844b369beaaa26ea39b55d
Seok Yim <seokyim8@gmail.com>
2024-06-03 20:21:07 -0400

    Add top navbar

---
+++ web_app/frontend/src/components/Leftbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Leftbar = () => {
+    return (
+        <div className='w-full md:flex'>
+            
+
+        </div>
+    );
+}
+
+export default Leftbar;
--- web_app/frontend/src/root/RootLayout.js
+++ web_app/frontend/src/root/RootLayout.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Topbar from '../components/Topbar';
+import Leftbar from '../components/Leftbar';
+import Bottombar from '../components/Bottombar';
+import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
     return (
-        <div>RootLayout</div>
+        <div className='w-full md:flex'>
+            <Topbar />
+            <Leftbar />
+
+            <section className='flex flex-1 h-full'>
+                <Outlet />
+            </section>
+            
+            <Bottombar />
+
+        </div>
     );
 }
 
