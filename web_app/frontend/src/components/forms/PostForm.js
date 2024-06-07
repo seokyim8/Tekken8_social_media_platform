02648ab644560b0c45f1946df97b47a339648ce5
Seok Yim <seokyim8@gmail.com>
2024-06-07 16:31:13 -0400

    Add image change functionalitry to create-post

--- web_app/frontend/src/components/forms/PostForm.js
+++ web_app/frontend/src/components/forms/PostForm.js
@@ -6,14 +6,18 @@ const PostForm = () => {
     const [imageLoaded, setImageLoaded] = useState(false);
 
 
-    const onImageChange = (event) => {
+    const onSetImage = (event) => {
         if (event.target.files && event.target.files[0]) {
             // Create image url
             setImage(URL.createObjectURL(event.target.files[0]));
             // Toggle image loaded status to determine what should be shown in the display area
             setImageLoaded((prevState) => !prevState);
         }
-        console.log(imageLoaded)
+    }
+
+    const onChangeImage = (event) => {
+        setImage(null);
+        setImageLoaded((prevState) => !prevState);
     }
 
     return (
@@ -27,7 +31,10 @@ const PostForm = () => {
                 <span className="block mt-4 mb-2 text-sm font-medium text:gray-900">Add Images</span>
                 <div className="flex items-center justify-center w-full">
                     {imageLoaded ? (
-                        <img src={image} />
+                        <div className="flex flex-col">
+                            <img src={image} />
+                            <button type="button" onClick={onChangeImage} className="my-2 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">Change Image</button>
+                        </div>
                     ) : (
                         <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                             <div className="flex flex-col items-center justify-center pt-5 pb-6">
@@ -37,7 +44,7 @@ const PostForm = () => {
                                 <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
                                 <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                             </div>
-                            <input id="dropzone-file" type="file" onChange={onImageChange} className="hidden" />
+                            <input id="dropzone-file" type="file" onChange={onSetImage} className="hidden" />
                         </label>
                     )}
                 </div>
